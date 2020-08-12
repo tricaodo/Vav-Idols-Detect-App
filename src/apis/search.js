698f01435eb4d624a4062aeb6bb23ac07eb87934
Tri Do <trido1903@gmail.com>
2020-08-12 11:01:36 -0700

    added git ignore apis called

--- src/apis/search.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://searchidols.cognitiveservices.azure.com/bing/v7.0/images",
-    headers: { "Ocp-Apim-Subscription-Key": "e47af8b5a8344a46bf67ceaed1146531" }
-});
